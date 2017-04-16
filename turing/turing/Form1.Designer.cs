e70e7a8a05520c318758d9202d6c638734b4be04
Zavodov <andrey0398@gmail.com>
2017-04-17 00:24:38 +0300

    Day 2
    + Добавлена проверка введенных данных
    + Добавлена возможность управлять лентой
    + Реализована структура для хранения ленты

--- turing/turing/Form1.Designer.cs
+++ turing/turing/Form1.Designer.cs
@@ -28,17 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.alphabet = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.progGrid = new System.Windows.Forms.DataGridView();
             this.lenta = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
@@ -48,15 +44,16 @@
             ((System.ComponentModel.ISupportInitialize)(this.lenta)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // alphabet
             // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.alphabet.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(86, 144);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(438, 23);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.Text = "01";
+            this.alphabet.Location = new System.Drawing.Point(86, 144);
+            this.alphabet.Name = "alphabet";
+            this.alphabet.Size = new System.Drawing.Size(438, 23);
+            this.alphabet.TabIndex = 0;
+            this.alphabet.Text = "_012";
+            this.alphabet.TextChanged += new System.EventHandler(this.alphabet_TextChanged);
             // 
             // label1
             // 
@@ -77,91 +74,79 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.progGrid.BackgroundColor = System.Drawing.Color.AliceBlue;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.Color.Silver;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.Silver;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.progGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Silver;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.Silver;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.progGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.progGrid.ColumnHeadersHeight = 25;
             this.progGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
-            this.progGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Column1,
-            this.Column2,
-            this.Column3,
-            this.Column4});
             this.progGrid.Location = new System.Drawing.Point(9, 172);
-            this.progGrid.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.progGrid.Margin = new System.Windows.Forms.Padding(2);
+            this.progGrid.MultiSelect = false;
             this.progGrid.Name = "progGrid";
             this.progGrid.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
             this.progGrid.RowHeadersWidth = 25;
+            this.progGrid.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
             this.progGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
             this.progGrid.Size = new System.Drawing.Size(296, 229);
             this.progGrid.TabIndex = 2;
+            this.progGrid.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.progGrid_CellValueChanged);
             this.progGrid.Resize += new System.EventHandler(this.progGrid_Resize);
             // 
             // lenta
             // 
+            this.lenta.AllowUserToAddRows = false;
+            this.lenta.AllowUserToDeleteRows = false;
+            this.lenta.AllowUserToResizeColumns = false;
+            this.lenta.AllowUserToResizeRows = false;
             this.lenta.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lenta.BackgroundColor = System.Drawing.Color.AliceBlue;
             this.lenta.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.lenta.ColumnHeadersVisible = false;
             this.lenta.Location = new System.Drawing.Point(36, 61);
-            this.lenta.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.lenta.Margin = new System.Windows.Forms.Padding(2);
+            this.lenta.MultiSelect = false;
             this.lenta.Name = "lenta";
+            this.lenta.RowHeadersVisible = false;
             this.lenta.ScrollBars = System.Windows.Forms.ScrollBars.None;
             this.lenta.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
             this.lenta.Size = new System.Drawing.Size(462, 78);
             this.lenta.TabIndex = 3;
+            this.lenta.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.lenta_CellValueChanged);
             this.lenta.Resize += new System.EventHandler(this.lenta_Resize);
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(9, 61);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(23, 78);
             this.button1.TabIndex = 4;
             this.button1.Text = "<";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
             this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.button2.Location = new System.Drawing.Point(501, 61);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(23, 78);
             this.button2.TabIndex = 5;
             this.button2.Text = ">";
             this.button2.UseVisualStyleBackColor = true;
-            // 
-            // Column1
-            // 
-            this.Column1.HeaderText = "Q1";
-            this.Column1.Name = "Column1";
-            // 
-            // Column2
-            // 
-            this.Column2.HeaderText = "Q2";
-            this.Column2.Name = "Column2";
-            // 
-            // Column3
-            // 
-            this.Column3.HeaderText = "Q3";
-            this.Column3.Name = "Column3";
-            // 
-            // Column4
-            // 
-            this.Column4.HeaderText = "Q4";
-            this.Column4.Name = "Column4";
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
             this.button3.Location = new System.Drawing.Point(9, 11);
-            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(128, 31);
             this.button3.TabIndex = 6;
@@ -171,7 +156,7 @@
             // button4
             // 
             this.button4.Location = new System.Drawing.Point(142, 11);
-            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(128, 31);
             this.button4.TabIndex = 7;
@@ -181,8 +166,9 @@
             // button5
             // 
             this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5.Enabled = false;
             this.button5.Location = new System.Drawing.Point(400, 11);
-            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button5.Margin = new System.Windows.Forms.Padding(2);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(124, 31);
             this.button5.TabIndex = 8;
@@ -196,7 +182,7 @@
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 15;
             this.listBox1.Location = new System.Drawing.Point(309, 201);
-            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(216, 199);
             this.listBox1.TabIndex = 9;
@@ -229,7 +215,7 @@
             this.Controls.Add(this.lenta);
             this.Controls.Add(this.progGrid);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.alphabet);
             this.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.MinimumSize = new System.Drawing.Size(552, 447);
             this.Name = "Form1";
@@ -244,13 +230,9 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox alphabet;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView progGrid;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
         private System.Windows.Forms.DataGridView lenta;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
--- turing/turing/Form1.cs
+++ turing/turing/Form1.cs
@@ -13,7 +13,30 @@ namespace turing
     public partial class Form1 : Form
     {
 
-        const int ROWSIZE = 27;
+        public struct cell
+        {
+            public string value;
+            public int index;
+
+            public cell(string value, int index)
+            {
+                this.value = value;
+                this.index = index;
+            }
+        }
+
+
+        const int COLSIZE = 30;
+
+        const int PROG_Y = 4;
+        const int LENT_BEG = -7;
+
+
+        public List<cell> item = new List<cell>();
+        protected int head = 0;
+        protected int begn = LENT_BEG;
+        protected string ABC = "_";
+
 
 
         public Form1()
@@ -21,26 +44,21 @@ namespace turing
             InitializeComponent();
         }
 
-        public void LentaPrint()
+        public void LentaPrint(int nach, int head)
         {
-            lenta.RowHeadersVisible = false;
-            lenta.ColumnHeadersVisible = false;
-            lenta.AllowUserToAddRows = false;
-            lenta.AllowUserToDeleteRows = false;
-            lenta.AllowUserToResizeColumns = false;
-            lenta.AllowUserToResizeRows = false;
-            lenta.ColumnCount = lenta.Width / ROWSIZE;
+            lenta.ColumnCount = lenta.Width / COLSIZE;
             lenta.RowCount = 3;
-            for (int i = 0; i < lenta.Columns.Count; i++)
-            {
-                int number = -(lenta.Columns.Count / 2) + i;                
-                lenta.Columns[i].Width = ROWSIZE;
+            int konc = nach + lenta.ColumnCount;
+            for (int i = 0; i < lenta.ColumnCount; i++)
+            {                               
+                lenta.Columns[i].Width = COLSIZE;
                 lenta.Rows[0].Cells[i].ReadOnly = true;
                 lenta.Rows[1].Cells[i].ReadOnly = true;              
-                if (number == 0) lenta.Rows[0].Cells[i].Value = "\\/";
+                if ((nach + i) == head) lenta.Rows[0].Cells[i].Value = "\\/";
                     else lenta.Rows[0].Cells[i].Value = "";
-                lenta.Rows[1].Cells[i].Value = number;
-                lenta.Rows[2].Cells[i].Value = "";
+                lenta.Rows[1].Cells[i].Value = nach + i;
+                lenta.Rows[1].Cells[i].Style.BackColor = Color.MintCream;
+                lenta.Rows[2].Cells[i].Value = GetValue(nach + i);
             }
 
             for (int i = 0; i < lenta.Rows.Count; i++)
@@ -48,9 +66,10 @@ namespace turing
 
         }
 
-        public void ProgGridPrint(int col, int row)
+        public void ProgGridPrint(int row)
         {
-            progGrid.ColumnCount = col;
+            progGrid.ColumnCount = alphabet.Text.Length;
+            var col = progGrid.ColumnCount;
             progGrid.RowCount = row;
 
             int colSize = (progGrid.Width - progGrid.ColumnHeadersHeight - 2) / col;
@@ -58,9 +77,11 @@ namespace turing
 
             for (int i = 0; i < progGrid.Columns.Count; i++)
             {
-                progGrid.Columns[i].HeaderText = "Q" + (i+1);
+                progGrid.Columns[i].HeaderText = alphabet.Text[i].ToString();
                 progGrid.Columns[i].Width = colSize;
-                
+                progGrid.Columns[i].SortMode = DataGridViewColumnSortMode.NotSortable;
+
+
             }
 
             for (int i = 0; i < progGrid.Rows.Count; i++)
@@ -70,25 +91,118 @@ namespace turing
 
             }
                 
-
-
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            ProgGridPrint(4,4);
-            LentaPrint();
-
+            ProgGridPrint(PROG_Y);
+            LentaPrint(begn, head);
+            ABC = alphabet.Text;
         }
 
         private void lenta_Resize(object sender, EventArgs e)
         {
-            LentaPrint();
+            LentaPrint(begn, head);
         }
 
         private void progGrid_Resize(object sender, EventArgs e)
         {
-            ProgGridPrint(4, 4);
+            ProgGridPrint(PROG_Y);
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            head++;
+            begn++;
+            LentaPrint(begn, head);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            head--;
+            begn--;
+            LentaPrint(begn, head);
+        }
+
+        private void progGrid_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            int x = progGrid.CurrentCell.RowIndex;
+            int y = progGrid.CurrentCell.ColumnIndex;
+
+            var cell = progGrid.Rows[x].Cells[y];                
+            var cValue = (string)cell.Value;
+                if (cValue == null) return;
+            
+            if (!(cValue.Length == 0 || cValue.Length == 4))
+                { cell.Value = ""; return; }
+            // if ()    
+
+        }
+
+        private void lenta_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            int x = lenta.CurrentCell.RowIndex;
+            int y = lenta.CurrentCell.ColumnIndex;
+
+            var cell = lenta.Rows[x].Cells[y];
+            var cValue = (string)cell.Value;
+
+            if (cValue.Length > 1) cell.Value = "";
+                else
+                if (!ABC.Contains(cValue)) cell.Value = "";
+
+            AddData (cValue, begn+y);
+        }
+
+        public void AddData(string value, int index)
+        {
+
+            int memInd = -1;
+            for (int i = 0; i < item.Count - 1; i++)
+                if (item[i].index == index)
+                {
+                    memInd = i;
+                    break;
+                }
+
+            if (memInd > -1) item[memInd] = new cell(value, index);
+            else
+                item.Add(new cell(value, index));
+        }
+
+        public string GetValue(int index)
+        {
+            foreach (var i in item)
+             if (i.index == index)
+               return i.value;
+
+            AddData("", index);
+            return "";
+        }
+
+        private void alphabet_TextChanged(object sender, EventArgs e)
+        {
+            var text = alphabet.Text;
+                if (text == "") { alphabet.Text = "_"; return; }                                 
+            var sim = text[text.Length-1];
+
+            bool flag = false;
+            foreach (var s in text)
+                if (s == '_') { flag = true; break; }
+            if (!flag) { text = "_" + text; alphabet.Text = text; }    
+                
+            for (int i = 0; i < text.Length-1; i++)
+              for (int j = i+1; j < text.Length; j++)
+                if (text[i] == text[j])
+                    {
+                        alphabet.Text = ABC;
+                        return;
+                    }
+                    
+            ABC = alphabet.Text;
+            ProgGridPrint(PROG_Y);         
+        }
+
+        
     }
 }
